50b014da49231856ec4a6d15a18d30fbc912fbc8
Sean O'Brien <sobri016@uottawa.ca>
2021-04-15 21:27:45 -0400

    added gradient algorithm

--- gradientTest.py
+++ gradientTest.py
@@ -1,7 +1,7 @@
-
+import matplotlib.pyplot as plt
 import psycopg2
 import numpy as np
-from sklearn import tree
+from sklearn import datasets, ensemble
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
@@ -50,13 +50,31 @@ print(len(X_train))
 print(len(X_test))
 print(len(y_train))
 print(len(y_test))
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X_train,y_train)
-mse = mean_squared_error(y_test, clf.predict(X_test))
+params = {'n_estimators': 500,
+          'max_depth': 4,
+          'min_samples_split': 5,
+          'learning_rate': 0.01,
+          'loss': 'ls'}
+reg = ensemble.GradientBoostingRegressor(**params)
+reg.fit(X_train, y_train)
+mse = mean_squared_error(y_test, reg.predict(X_test))
 print("The mean squared error (MSE) on test set: {:.4f}".format(mse))
-#dot_data = tree.export_graphviz(clf, out_file=None)
-#graph = graphviz.Source(dot_data)
-#graph.render("treeModel")
+test_score = np.zeros((params['n_estimators'],), dtype=np.float64)
+for i, y_pred in enumerate(reg.staged_predict(X_test)):
+    test_score[i] = reg.loss_(y_test, y_pred)
+
+fig = plt.figure(figsize=(6, 6))
+plt.subplot(1, 1, 1)
+plt.title('Deviance')
+plt.plot(np.arange(params['n_estimators']) + 1, reg.train_score_, 'b-',
+         label='Training Set Deviance')
+plt.plot(np.arange(params['n_estimators']) + 1, test_score, 'r-',
+         label='Test Set Deviance')
+plt.legend(loc='upper right')
+plt.xlabel('Boosting Iterations')
+plt.ylabel('Deviance')
+fig.tight_layout()
+plt.show()
 
 cur.close()
 conn.close()
